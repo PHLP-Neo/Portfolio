16d53ce45c6375b560ae6b2d478f483d94e24f05
PHLP-Neo <phlp.neo@gmail.com>
2026-04-20 15:17:09 +1000

    change image recognition model to best model

--- Monash Course Work/FIT5225/A1/Plastic-Detection-in-River/main.py
+++ Monash Course Work/FIT5225/A1/Plastic-Detection-in-River/main.py
@@ -8,7 +8,7 @@ from ultralytics import YOLO
 app = FastAPI()
 
 # 1. 
-model = YOLO("yolov8m.pt")
+model = YOLO("runs/detect/train/weights/best.pt")
 
 # 2.  [cite: 206, 207]
 class PredictRequest(BaseModel):
@@ -21,7 +21,7 @@ async def predict(request: PredictRequest):
     header, encoded = request.image.split(",", 1) if "," in request.image else ("", request.image)
     img_bytes = base64.b64decode(encoded)
     nparr = np.frombuffer(img_bytes, np.uint8)
-    img = cv2.imdecode(nparr, cv2.imread_color)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
     # 4. 
     results = model.predict(source=img)
@@ -32,7 +32,13 @@ async def predict(request: PredictRequest):
         "uuid": request.uuid,
         "count": len(res.boxes),
         "detections": [res.names[int(c)] for c in res.boxes.cls],
-        "boxes": [], # 
+        "boxes": [
+            {
+                "box": [float(x) for x in b.xyxy[0]], # [x1, y1, x2, y2]
+                "confidence": float(b.conf[0]),
+                "label": res.names[int(b.cls[0])]
+            } for b in res.boxes
+        ], # 
         "speed_preprocess_ms": res.speed['preprocess'],
         "speed_inference_ms": res.speed['inference'],
         "speed_postprocess_ms": res.speed['postprocess']
