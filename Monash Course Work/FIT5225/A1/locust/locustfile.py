5d204dee9481208e40b43843e892516cdea3e518
PHLP-Neo <phlp.neo@gmail.com>
2026-04-25 22:33:50 +1000

    locust run at horrible time, time for optimization

--- Monash Course Work/FIT5225/A1/locust/locustfile.py
+++ Monash Course Work/FIT5225/A1/locust/locustfile.py
@@ -1,12 +1,17 @@
+import base64
+import uuid
 from locust import HttpUser, task, between
 
 class PlasticApiUser(HttpUser):
     wait_time = between(1, 5)
 
     @task
-    def predict_plastic(self):
+    def predict_image(self):
         image_path = "test.jpg" 
-        
-        with open(image_path, "rb") as image_file:
-            files = {'file': (image_path, image_file, 'image/jpeg')}
-            self.client.post("/predict", files=files)
+        with open(image_path, "rb") as f:
+            img_str = base64.b64encode(f.read()).decode('utf-8')
+        payload = {
+            "uuid": str(uuid.uuid4()), 
+            "image": img_str          
+        }
+        self.client.post("/api/predict", json=payload)
