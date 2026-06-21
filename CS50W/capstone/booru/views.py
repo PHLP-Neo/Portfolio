beb6f342906f0a931897f37fa213b2935c5f3bbe
PHLP-Neo <phlp.neo@gmail.com>
2026-06-22 01:38:45 +1000

    created first views

--- CS50W/capstone/booru/views.py
+++ CS50W/capstone/booru/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ImagePost
 
 # Create your views here.
+
+
+def gallery(request):
+    posts = ImagePost.objects.all().order_by("-created_at")
+    return render(request, "booru/gallery.html", {
+        "posts": posts
+    })
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(ImagePost, id=post_id)
+    return render(request, "booru/post_detail.html", {
+        "post": post
+    })
