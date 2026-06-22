06e1208c396978f9fe053a2de1f79f445bc0e077
PHLP-Neo <phlp.neo@gmail.com>
2026-06-22 15:37:03 +1000

    Register favouritede model to admin

--- CS50W/capstone/booru/admin.py
+++ CS50W/capstone/booru/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import ImagePost, Tag
+from .models import ImagePost, Tag, Favorite
 
 # Register your models here.
 admin.site.register(ImagePost)
 admin.site.register(Tag)
+admin.site.register(Favorite)
