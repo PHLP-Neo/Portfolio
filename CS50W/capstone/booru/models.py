4dc46713dbbb4d6ec898404c596cd9f2a4736139
PHLP-Neo <phlp.neo@gmail.com>
2026-06-22 15:48:13 +1000

    Added comment model

--- CS50W/capstone/booru/models.py
+++ CS50W/capstone/booru/models.py
@@ -30,3 +30,12 @@ class Favorite(models.Model):
 
     def __str__(self):
         return f"{self.user.username} favorited {self.post.title}"
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(ImagePost, on_delete=models.CASCADE, related_name="comments")
+    content = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} on {self.post.title}"
