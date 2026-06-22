7641f6e0895566e38f485a801237f8c07889c700
PHLP-Neo <phlp.neo@gmail.com>
2026-06-22 13:44:12 +1000

    Added upload URL

--- CS50W/capstone/booru/urls.py
+++ CS50W/capstone/booru/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.gallery, name="gallery"),
     path("post/<int:post_id>/", views.post_detail, name="post_detail"),
+    path("upload/", views.upload_post, name="upload_post"),
 ]
