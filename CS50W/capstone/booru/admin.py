85dbe07fc9f21b0d364dafcfee8673fb05fce40a
PHLP-Neo <phlp.neo@gmail.com>
2026-06-27 18:20:14 +1000

    remove everything, it is a lost cause

--- CS50W/capstone/booru/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import ImagePost, Tag, Favorite, Comment
-
-# Register your models here.
-admin.site.register(ImagePost)
-admin.site.register(Tag)
-admin.site.register(Favorite)
-admin.site.register(Comment)
--- CS50W/capstone/capstone/urls.py
+++ CS50W/capstone/capstone/urls.py
@@ -15,19 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-# ^ setting up mediafile based on this post:
-# https://dev.to/cleverzone/a-beginner-guide-to-upload-images-in-a-rest-api-with-django-opf
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("", include("booru.urls")),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# ^ setting up mediafile based on this post:
-# https://dev.to/cleverzone/a-beginner-guide-to-upload-images-in-a-rest-api-with-django-opf
