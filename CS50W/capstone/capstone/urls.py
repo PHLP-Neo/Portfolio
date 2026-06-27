c1d4a95b0c9e08e4db4210c2385b9c0a8ab8a26b
PHLP-Neo <phlp.neo@gmail.com>
2026-06-27 17:06:39 +1000

    added documentations from stackoverflow

--- CS50W/capstone/capstone/urls.py
+++ CS50W/capstone/capstone/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+# ^ setting up mediafile based on this post:
+# https://stackoverflow.com/questions/5517950/django-media-url-and-media-root
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +29,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# ^ setting up mediafile based on this post:
+# https://stackoverflow.com/questions/5517950/django-media-url-and-media-root
