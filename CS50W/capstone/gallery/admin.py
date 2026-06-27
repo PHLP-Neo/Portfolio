d0d1235c509059869376fd7f4dee0ca2c73ac5f1
PHLP-Neo <phlp.neo@gmail.com>
2026-06-27 18:46:23 +1000

    setup admin stats

--- CS50W/capstone/gallery/admin.py
+++ CS50W/capstone/gallery/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, ImagePost, Tag, Comment
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(ImagePost)
+admin.site.register(Tag)
+admin.site.register(Comment)
