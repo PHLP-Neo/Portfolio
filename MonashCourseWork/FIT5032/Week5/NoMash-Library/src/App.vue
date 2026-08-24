dc44e0f75ca4cc9a36f2bf4131fd0f676294ba2c
PHLP-Neo <phlp.neo@gmail.com>
2026-08-24 20:20:27 +1000

    update for task 5

--- MonashCourseWork/FIT5032/Week5/NoMash-Library/src/App.vue
+++ MonashCourseWork/FIT5032/Week5/NoMash-Library/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import JSONLab from './components/JSONLab.vue'
 import BHeader from './components/BHeader.vue'
-import LibraryRegistrationForm from './views/HomeView.vue/index.js'
+//import LibraryRegistrationForm from './views/HomeView.vue/index.js'
 </script>
 
 <template>
@@ -10,8 +10,9 @@ import LibraryRegistrationForm from './views/HomeView.vue/index.js'
   </header>
 
   <main>
-    <LibraryRegistrationForm />
+    <!-- <LibraryRegistrationForm /> -->
     <!-- <JSONLab /> -->
+    <router-view></router-view>
   </main>
 </template>
 
--- MonashCourseWork/FIT5032/Week5/NoMash-Library/src/main.js
+++ MonashCourseWork/FIT5032/Week5/NoMash-Library/src/main.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import PrimeVue from 'primevue/config'
 import Aura from '@primevue/themes/aura'
@@ -15,5 +16,7 @@ app.use(PrimeVue, { theme: { preset: Aura } })
 
 // app.component('DataTable', DataTable)
 // app.component('Column', Column)
+
+app.use(router)
 
 app.mount('#app')
